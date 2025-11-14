d07e14ca25987332ab90fc53618437325d4e6d4d
saikey <gintama10283291@gmail.com>
2025-11-14 20:24:24 +0900

    yaml 채널별 전처리 추가

--- object_detection/reid_repVGG_ov.py
+++ object_detection/reid_repVGG_ov.py
@@ -12,11 +12,17 @@ class OVReID:
         self.input = self.compiled.inputs[0]
         self.output = self.compiled.outputs[0]
 
+        # Hailo yaml에서 가져온 전처리 값 (RGB)
+        self.mean = np.array([111.296, 104.263, 104.396], dtype=np.float32)
+        self.std  = np.array([64.193, 62.547, 60.324], dtype=np.float32)
+        
     def embed(self, crop_bgr):
         img = cv2.resize(crop_bgr, (128,256))
-        rgb = img[:,:,::-1]
-        tensor = rgb.transpose(2,0,1)[None].astype(np.float32)  # normalize if needed
-        emb = self.compiled([tensor])[self.output]
+        rgb = img[:, :, ::-1].astype(np.float32)  # BGR -> RGB
+        rgb = (rgb - self.mean) / self.std        # 채널별 정규화 적용
+        
+        tensor = rgb.transpose(2,0,1)[None].astype(np.float32)  # NCHW (1, 3, 256, 128)
+        emb = self.compiled([tensor])[self.output] 
         emb = emb.astype(np.float32).squeeze()
-        n = np.linalg.norm(emb) + 1e-6
+        n = np.linalg.norm(emb) + 1e-6                          # L2 정규화 (코사인 거리용)
         return emb / n
